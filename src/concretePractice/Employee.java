c4b03cf6a3f6a5d1a3772de1ccd430732f13d63b
F03 H4MM3R <F03 H4MM3R@10.30.30.97>
2013-08-27 16:37:53 -0500

    created 3 packages, concrete, abstract, and interface. created Employee class in concrete.

---
+++ src/concretePractice/Employee.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package concretePractice;
+
+/**
+ *
+ * @author F03 H4MM3R
+ */
+public class Employee {
+    
+}
---
+++ src/inheritancepractice2/InheritancePractice2.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package inheritancepractice2;
+
+/**
+ *
+ * @author F03 H4MM3R
+ */
+public class InheritancePractice2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
