fd9f156c4972331c1b7df9ef632c6312c535fe7c
F03 H4MM3R <F03 H4MM3R@10.30.30.97>
2013-08-28 15:46:53 -0500

    created all four classes for abstractPractice

---
+++ src/abstractPractice/Employee.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package abstractPractice;
+
+/**
+ *
+ * @author F03 H4MM3R
+ */
+public class Employee {
+    
+}
