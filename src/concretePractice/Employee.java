a4a58a5b39cdb8c80a62bfdccd1caa7736b2e489
F03 H4MM3R <F03 H4MM3R@10.30.30.97>
2013-08-27 20:37:50 -0500

    employee class getters and setters

--- src/concretePractice/Employee.java
+++ src/concretePractice/Employee.java
@@ -9,5 +9,79 @@ package concretePractice;
  * @author F03 H4MM3R
  */
 public class Employee {
-    
+    private String firstName;
+    private String lastName;
+    private String position;
+    private int hireDate;
+    private double employeeNumber;
+
+    /**
+     * @return the firstName
+     */
+    public String getFirstName() {
+        return firstName;
+    }
+
+    /**
+     * @param firstName the firstName to set
+     */
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    /**
+     * @return the lastName
+     */
+    public String getLastName() {
+        return lastName;
+    }
+
+    /**
+     * @param lastName the lastName to set
+     */
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    /**
+     * @return the position
+     */
+    public String getPosition() {
+        return position;
+    }
+
+    /**
+     * @param position the position to set
+     */
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    /**
+     * @return the hireDate
+     */
+    public int getHireDate() {
+        return hireDate;
+    }
+
+    /**
+     * @param hireDate the hireDate to set
+     */
+    public void setHireDate(int hireDate) {
+        this.hireDate = hireDate;
+    }
+
+    /**
+     * @return the employeeNumber
+     */
+    public double getEmployeeNumber() {
+        return employeeNumber;
+    }
+
+    /**
+     * @param employeeNumber the employeeNumber to set
+     */
+    public void setEmployeeNumber(double employeeNumber) {
+        this.employeeNumber = employeeNumber;
+    }
 }
